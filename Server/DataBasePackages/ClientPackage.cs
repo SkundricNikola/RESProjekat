971e70da41af88df132f98a07785c35620103baa
conelini <tepavcevic.pr14.2018@uns.ac.rs>
2021-05-25 01:15:31 +0200

    Create ClientPackage.cs

---
+++ Server/DataBasePackages/ClientPackage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBasePackages
+{
+    public class ClientPackage
+    {//Klasa ClientPackage sluzi kao forma koja ce se koristiti pri cuvanju u bazi podataka
+        private DateTime datum;
+        private string region;
+        private double potrosnja;
+        
+        public DateTime Datum
+        {
+            get { return datum; }
+            set { datum = value; }
+        }
+
+        public string Region
+        {
+            get { return region; }
+            set { region = value; }
+        }
+
+        public double Potrosnja
+        {
+            get { return potrosnja; }
+            set { potrosnja = value; }
+        }
+
+        public override string ToString()
+        {
+            string str = string.Format("{0}/{1}/{2}/{3}/{4}/{5}-{6}-{7}",
+                datum.Second,
+                datum.Minute,
+                datum.Hour,
+                datum.Day,
+                datum.Month,
+                datum.Year,
+                region,
+                potrosnja);
+            return str;
+
+        }
+    }
+}
