1c6e8008699900f0c5b06ff6ca9458015d4072b7
Tepica-admin <tepavcevic.pr14.2018@uns.ac.rs>
2021-06-06 17:37:18 +0200

    Update Client.cs

--- Client/Client/Client.cs
+++ Client/Client/Client.cs
@@ -52,7 +52,8 @@ namespace Client
                     }
                     else
                     {
-                        i.PrintList(responseData);
+                        //i.PrintList(responseData);
+                        Console.WriteLine(responseData);
                     }
                 }
                 stream.Close();
