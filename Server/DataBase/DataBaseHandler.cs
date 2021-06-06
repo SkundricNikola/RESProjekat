74533b166ef038ba7fca92be5facfb48a85dc2a3
Tepica-admin <tepavcevic.pr14.2018@uns.ac.rs>
2021-06-06 23:48:05 +0200

    Update DataBaseHandler.cs

--- Server/DataBase/DataBaseHandler.cs
+++ Server/DataBase/DataBaseHandler.cs
@@ -12,13 +12,13 @@ namespace DataBase
         public static void Main(string[] args)
         {
             
-            var th1 = new Thread(() => DataAccessCommunication.ReceiveMessage_t1());
-            var th3 = new Thread(() => DataAccessCommunication.ReceiveMessage_t3());
+            //var th1 = new Thread(() => DataAccessCommunication.ReceiveMessage_t1());
+            //var th3 = new Thread(() => DataAccessCommunication.ReceiveMessage_t3());
             var th2 = new Thread(() => DataAccessCommunication.ReceiveMessage_t2());
 
-            th1.Start();
+            //th1.Start();
             th2.Start();
-            th3.Start();
+            //th3.Start();
 
         }
     }
